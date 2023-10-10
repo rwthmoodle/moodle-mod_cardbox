5becee6f95aa13ef7a8c8b80254355809f1c8bde
Tim Schroeder <t.schroeder@itc.rwth-aachen.de>
2023-10-10 14:26:16 +0200

    new Release v1.0.4

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_cardbox';
-$plugin->version = 2023060200;
-$plugin->release = '1.0.3';
+$plugin->version = 2023101000;
+$plugin->release = '1.0.4';
 $plugin->requires = 2018120302.05;
-$plugin->maturity = MATURITY_RC;
+$plugin->maturity = MATURITY_STABLE;
